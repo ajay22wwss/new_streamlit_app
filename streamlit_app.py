d3a0aaf72dfbb310c14c19f1312f98b08668e69e
ajay22wwss <140741680+ajay22wwss@users.noreply.github.com>
2023-08-01 23:55:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,11 +15,10 @@ streamlit.dataframe(my_fruit_list)
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),'Avocado')
 
-streamlit.dataframe(my_fruit_list)
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.dataframe(my_fruit_list)
+
 
 
 
